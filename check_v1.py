51187e966e5b2b0066183ee09db746cdc6b3e9e6
parisote <detomas@gmail.com>
2022-06-21 00:21:46 -0300

    fix: add check function

--- check_v1.py
+++ check_v1.py
@@ -4,50 +4,56 @@ import re
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
-mail = os.getenv('EMAIL')
-
-if len(sys.argv) > 1:
-    mail = sys.argv[1]
-
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  
-if not re.search(regex,mail):
-    print('INVALID MAIL')
-    sys.exit(0)
-
-request_token = requests.get('https://rooftop-career-switch.herokuapp.com/token?email='+mail)
-TOKEN=request_token.json()['token']
-
-request_block = requests.get('https://rooftop-career-switch.herokuapp.com/blocks?token='+TOKEN)
-array_job = request_block.json()['data']
-original_size = len(array_job)
-
-#INCIALIZO
-array_order = []
-array_order.append(array_job.pop(0))
-copyA = array_job[::-1]
-
-def checkValue(v2):
-    last = array_order[-1]
-    block = {'blocks': [last,v2]}
+def checkValue(array_order,v2):
+    v1 = array_order[-1]
+    block = {'blocks': [v1,v2]}
     check = requests.post('https://rooftop-career-switch.herokuapp.com/check?token='+TOKEN, json=block)
     if check.json()['message']:
         array_order.append(v2)
         return True
     return False
 
-while len(array_order) != original_size:
-    for i in range(len(copyA)):
-        if copyA[i] in array_order:
-            continue
-        v2 = copyA[i]
-        if checkValue(v2):
-            break
+#O(n**2) :S
+def check(array_job):
+    original_size = len(array_job)
+    array_order = []
+    array_order.append(array_job.pop(0))
+    copyA = array_job[::-1]
 
-final={'encoded':''.join(array_order)}
-result = requests.post('https://rooftop-career-switch.herokuapp.com/check?token='+TOKEN, json=final)
+    while len(array_order) != original_size:
+        for i in range(len(copyA)):
+            if copyA[i] in array_order:
+                continue
+            v2 = copyA[i]
+            if checkValue(array_order,v2):
+                break
 
-print('ARRAY INCIAL: ', request_block.json()['data'])
-print('ARRAY ORDENADO: ', array_order)
-print('RESULTADO FINAL: ', result.json()['message'])
+    return array_order
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    mail = os.getenv('EMAIL')
+
+    if len(sys.argv) > 1:
+        mail = sys.argv[1]
+
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  
+    if not re.search(regex,mail):
+        print('INVALID MAIL')
+        sys.exit(0)
+
+    request_token = requests.get('https://rooftop-career-switch.herokuapp.com/token?email='+mail)
+    TOKEN=request_token.json()['token']
+
+    request_block = requests.get('https://rooftop-career-switch.herokuapp.com/blocks?token='+TOKEN)
+    array_job = request_block.json()['data']    
+
+    array_order = check(array_job)
+
+    final={'encoded':''.join(array_order)}
+    result = requests.post('https://rooftop-career-switch.herokuapp.com/check?token='+TOKEN, json=final)
+
+    print('ARRAY INCIAL: ', request_block.json()['data'])
+    print('ARRAY ORDENADO: ', array_order)
+    print('RESULTADO FINAL: ', result.json()['message'])
